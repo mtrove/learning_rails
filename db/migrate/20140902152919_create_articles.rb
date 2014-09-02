4ebad55bcb4f93c35f9d1fe35fab0acf2901654b
mtrove <yico1989@gmail.com>
2014-09-02 23:58:40 +0800

    add database before migrate database

--- db/migrate/20140902152919_create_articles.rb
+++ db/migrate/20140902152919_create_articles.rb
@@ -1,6 +1,9 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
+      t.string :title
+      t.text   :body
+      t.datetime :published__at
 
       t.timestamps
     end
