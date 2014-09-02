8c3ecea0a2e7415f6b8e0ab13a5c6685051b3ab7
mtrove <yico1989@gmail.com>
2014-09-02 23:38:29 +0800

    rails generate a model after create database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningRails::Application.config.secret_key_base = 'fa8b1b8684ee3b16d611538ace14b7721447c37848fcf734b9120cffa3493df1e4266dbe963b8ec1fc1d646aa95d2e168a535f60742422f1e4032c280cd4507c'
