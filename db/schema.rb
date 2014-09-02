3629ae940b85fab2b19224086929196ad6f60f07
mtrove <yico1989@gmail.com>
2014-09-03 00:00:20 +0800

    rake db:migrate after databse create

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140902152919) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "published__at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
