42f7dc1fd8dab760e63e0475bc70f4123336f8a4
mtrove <yico1989@gmail.com>
2014-09-03 23:19:51 +0800

    rails generate scaffold some database stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902152919) do
+ActiveRecord::Schema.define(version: 20140903145109) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140902152919) do
     t.datetime "published__at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "excerpt"
+    t.string   "location"
   end
 
 end
